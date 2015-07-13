2a471eb2357d9c44f707e2d0e52590da7b554b8e
Jordan Riser <riser.jordan@gmail.com>
2015-07-13 14:30:23 -0400

    annotated response function

--- app.js
+++ app.js
@@ -26,13 +26,16 @@ var client = new irc.client(clientOptions);
 client.connect();
 
 client.addListener('chat', function(channel, user, message) {
+  // Filter through commands and try to find one
   var command = Commands.filter(function(el) {
     return el.trigger === message.toLowerCase();
   });
   if(command[0]){
+    // A command was returned by our filter so respond to it
     var response = template(command[0].response, {user: user, channel: channel, message: message});
     client.say(channel, response);
   }else{
+    // A command was not found so let the user know
     var commandString = message.match(/!\w+/g);
     var error = template("Sorry ${command} is not a valid command.",{command: commandString});
     client.say(channel, error);
