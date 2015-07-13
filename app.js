ae70556c2aa69f86e3afb49cb53f5aa446987e16
Jordan Riser <riser.jordan@gmail.com>
2015-07-13 15:45:27 -0400

    added permission checks for commands

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ var config = require('./config');
 var irc = require('twitch-irc');
 var template = require('es6-template-strings');
 
+/**
+ ** Available User Roles **
+   staff
+   admin
+   broadcaster
+   global_mod
+   mod `Moderator status will be detected after joining a channel and may take up to 30-45 seconds. (like the chat on twitch.tv)`
+   subscriber
+   turbo
+**/
+
 var Commands = [
   {
     trigger: '!hello',
     response: "Hello ${user.username}!",
-    permission: 0
+    permission: null
+  },
+  {
+    trigger: '!broadcast',
+    response: "Only the broadcaster can use this command!",
+    permission: 'broadcaster'
   }
 ];
 
@@ -26,14 +42,45 @@ var client = new irc.client(clientOptions);
 client.connect();
 
 client.addListener('chat', function(channel, user, message) {
+  // Setup permission helpers
+  var isBroadcaster = false;
+  user.special.map(function(e){
+    if(e == 'broadcaster'){
+      isBroadcaster = true;
+    }
+  });
   // Filter through commands and try to find one
   var command = Commands.filter(function(el) {
     return el.trigger === message.toLowerCase();
   });
-  if(command[0]){
-    // A command was returned by our filter so respond to it
+  var respond = function() {
     var response = template(command[0].response, {user: user, channel: channel, message: message});
     client.say(channel, response);
+  };
+  console.log(user);
+  if(command[0]){
+    // A command was returned by our filter so check if user has permission
+    if(!command[0].permission || isBroadcaster){
+      // There is no permission restriction or the user is the broadcaster
+      respond();
+    }else{
+      // There is a permission so lets check if the user has a required permission
+      var hasPermission = false;
+      user.special.map(function(el){
+        if(el == command[0].permission){
+          hasPermission = true;
+        }else{
+          hasPermission = false;
+        }
+      });
+      if(hasPermission){
+        respond();
+      }else{
+        var permissionCommandString = message.match(/!\w+/g);
+        var permissionError = template("You do not have permission to use the ${command} command.",{command: permissionCommandString});
+        client.say(channel, permissionError);
+      }
+    }
   }else{
     // A command was not found so let the user know
     var commandString = message.match(/!\w+/g);
