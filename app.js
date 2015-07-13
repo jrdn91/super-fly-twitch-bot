45b5ee98312cc9130640828b8701ca670b99a6c8
Jordan Riser <riser.jordan@gmail.com>
2015-07-13 13:55:58 -0400

    moved commands into seperate variable and implemented search functionality

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var config = require('./config');
 var irc = require('twitch-irc');
 var template = require('es6-template-strings');
 
+var Commands = [
+  {
+    trigger: '!hello',
+    response: "Hello ${user.username}!",
+    permission: 0
+  }
+];
+
 var clientOptions = {
   options: {
     debug: true,
@@ -18,10 +26,9 @@ var client = new irc.client(clientOptions);
 client.connect();
 
 client.addListener('chat', function(channel, user, message) {
-  console.log(user);
-  console.log(message);
-  if(message.toLowerCase() == '!hello') {
-    var response = template("Hello ${user.username}!", {user: user});
-    client.say(channel, response);
-  }
+  var command = Commands.filter(function(el) {
+    return el.trigger === message.toLowerCase();
+  });
+  var response = template(command[0].response, {user: user, channel: channel, message: message});
+  client.say(channel, response);
 });
