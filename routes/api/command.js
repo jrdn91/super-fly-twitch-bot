d8f44373643b1aeb78034c98d1beae05cce14be6
Jordan Riser <riser.jordan@gmail.com>
2015-08-18 14:42:53 -0400

    added command api

---
+++ routes/api/command.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var Command = require('../../models/command');
+
+module.exports.addCommand = function(req, res) {
+  var command = new Command(req.body.command);
+
+  command.save(function(err) {
+    if (err)
+      res.send(err);
+    res.json({command: command});
+  });
+};
+
+module.exports.getAllCommands = function(req, res) {
+  Command.find(function(err, commands) {
+    if (err) {
+      res.send(err);
+    }
+    res.json({commands: commands});
+  });
+};
+
+module.exports.updateCommand = function(req, res, id) {
+  Command.findByIdAndUpdate(id, {$set: req.body.command}, function(err, command) {
+    if (err) {
+      res.send(err);
+    }
+    res.json({command: command});
+  });
+};
+
+module.exports.deleteCommand = function(req, res, id) {
+  Command.findByIdAndRemove(id, function(err) {
+    if (err) {
+      res.send(err);
+    }
+    res.sendStatus(200);
+  });
+};
