f41e22b2c5bfc4ae5038ebe0c822559b7ed58030
Jordan Riser <riser.jordan@gmail.com>
2015-08-18 11:11:26 -0400

    fixed access controll settings

--- server.js
+++ server.js
@@ -16,11 +16,17 @@ app.use(bodyParser.urlencoded({
 
 // CORS
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Content-Length, X-Requested-With');
 
-    next();
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
 };
 app.use(allowCrossDomain);
 
