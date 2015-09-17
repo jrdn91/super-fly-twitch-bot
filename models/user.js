783f3563d48e48c206161a7e07c70e265ae81704
Jordan Riser <riser.jordan@gmail.com>
2015-09-17 12:51:43 -0400

    added defaults for minutes and currency for when creating a new user

--- models/user.js
+++ models/user.js
@@ -5,8 +5,8 @@ var timestamps = require('mongoose-timestamp');
 // Define our beer schema
 var UserSchema   = new mongoose.Schema({
   username: String,
-  minutes: Number,
-  currency: Number,
+  minutes: {type: Number, default: 0},
+  currency: {type: Number, default: 0},
 });
 
 UserSchema.plugin(timestamps,{ createdAt: "created_at", updatedAt: "updated_at" });
